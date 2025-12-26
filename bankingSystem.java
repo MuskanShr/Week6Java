1708ffb6812e391fdfbc9da49e05778194fed7e6
MuskanShr <muskanshr014@gmail.com>
2025-12-26 12:55:22 +0545

    My file contain banking system

---
+++ bankingSystem.java
@@ -0,0 +1,21 @@
+package example;
+
+public class bankingSystem {
+	public double balance = 0;
+	
+	public void deposit(double amt) {
+		balance += amt;
+	}
+	public void withdraw(double amt) {
+		balance -= amt;
+	}
+	   public double getBalance() {
+	        return balance;
+}
+}
+class myName{
+	public void name() {
+		System.out.println("Muskan Shrestha");
+	}
+}
+
